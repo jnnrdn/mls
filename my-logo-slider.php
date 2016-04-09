df9decfb7fc1d5ae5fa4c5eb343921394f2ed158
Jenny <jenny@webbviken.se>
2016-04-09 22:28:55 +0200

    Add widget structure code from https://premium.wpmudev.org/blog/featured-content-widget/.

--- my-logo-slider.php
+++ my-logo-slider.php
@@ -15,4 +15,30 @@ License: GNU General Public License v2.0 (or later)
 License URI: http://www.opensource.org/licenses/gpl-license.php
 */
 
+add_action( 'widgets_init', 'mls_init' );
 
+function mls_init() {
+	register_widget( 'mls_widget' );
+}
+
+class mls_widget extends WP_Widget
+{
+
+    public function __construct()
+    {
+        // Basic widget details
+    }
+
+    public function widget( $args, $instance )
+    {
+        // Widget output in the front end
+    }
+
+    public function update( $new_instance, $old_instance ) {
+        // Form saving logic - if needed
+    }
+
+    public function form( $instance ) {
+        // Backend Form
+    }
+}
